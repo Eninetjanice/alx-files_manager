5a39755ef2777ad75086b6299d41f42ddf8e173d
Janeth Eni <enijaneth@gmail.com>
2023-06-01 04:31:54 +0100

    Image Thumbnails

---
+++ woker.js
@@ -0,0 +1,39 @@
+const Queue = require('bull');
+const thumbnail = require('image-thumbnail');
+const dbClient = require('./utils/db');
+
+const fileQueue = new Queue('fileQueue');
+
+fileQueue.process(async (job) => {
+  const { fileId = null, userId = null } = job.data;
+
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+
+  const files = dbClient.db.collection('files');
+  const file = await files.findOne({ _id: fileId, userId });
+
+  if (!file) {
+    throw new Error('File not found');
+  }
+
+  const originalFilePath = file.localPath;
+
+  const thumbnailSizes = [500, 250, 100];
+  const thumbnailPromises = thumbnailSizes.map(async (size) => {
+    const thumbnailPath = `${originalFilePath}_${size}`;
+    const thumbnailOptions = { width: size };
+
+    const thumbnailData = await thumbnail(originalFilePath, thumbnailOptions);
+    await fs.writeFile(thumbnailPath, thumbnailData);
+  });
+
+  await Promise.all(thumbnailPromises);
+});
+
+module.exports = fileQueue;
